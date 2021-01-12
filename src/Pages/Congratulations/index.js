93c3c25cf724f5b70fe4d183085877c4c369a9dd
thihuli <thiagocandidof@hotmail.com>
2021-01-12 00:48:22 -0300

    add page congratulations

---
+++ src/Pages/Congratulations/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+
+function Congratulations() {
+  return (
+    <S.Container>
+      <h1>Parabéns pelas compras! Até breve.</h1>
+    </S.Container>
+  );
+}
+
+export default Congratulations;
